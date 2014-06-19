4617ab2ba539e2f27a10a8196d4bb554d39aaaa8
Reto Stamm <reto.stamm@originate.com>
2014-06-19 16:47:24 -0700

    Fixed Sherlock.

--- testapp/src/main/java/com/originate/testapp/testapp/MainActivity.java
+++ testapp/src/main/java/com/originate/testapp/testapp/MainActivity.java
@@ -1,12 +1,12 @@
 package com.originate.testapp.testapp;
 
-import android.support.v7.app.ActionBarActivity;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends SherlockFragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,23 +14,4 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
